76e36863aec9b8e41bf3572d507d2e121fb7fca0
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-11-13 01:18:51 +0700

    Webhook

---
+++ type.go
@@ -0,0 +1,14 @@
+package webhook
+
+type Reply struct {
+	Message string `bson:"messsage"`
+}
+
+var stringreply = []string{
+	"Jangan SPAAM woiii berisik lagi turuuuuu",
+	"Kamu hensommm bgt tau",
+	"genchananaaa genchanaaaayoooo",
+	"Jangan galak dong, memet takut tauu",
+	"uhuyyy hari ini memet pusingggg banget",
+	"Bercandyaaa berrcandaayoooooooooooo",
+}
